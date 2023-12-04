58639cf0205fca5c60280a8dc3873ca53cda1cd5
Chernoboysss <Mhmffadillah1@gmail.com>
2023-12-04 23:45:20 +0700

    commit kedua

--- review.php
+++ review.php
@@ -11,68 +11,11 @@ include 'header.php';
     </div>
 </section>
 <?php
-<<<<<<< HEAD
-// connect ke database sementara
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "db_carsresent";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-//echo "Connected successfully";
-$id_user = $_REQUEST['nama'] ?? null;
-$id_mobil = $_REQUEST['mobil'] ?? null;
-$review =  $_REQUEST['review'] ?? null;
-
-
-// Performing insert query execution
-// here our table name is college
-
-$submit = "INSERT INTO review (id_user,id,massage)  VALUES ('$id_user','$id_mobil','$review')";
-mysqli_query($conn, $submit)
-
-?>
-<div class="container mt-5 mb-5">
-    <div class="section-title mb-4 mb-xl-0 mt-5" data-aos="fade-left" data-aos-duration="1000">
-        <div class="row row-gap-3">
-            <tbody>
-                <?php
-                // connect ke database sementara
-                $servername = "localhost";
-                $username = "root";
-                $password = "";
-                $dbname = "db_carsresent";
-=======
->>>>>>> a7326631bc79ff171851a12f6ce9dc3586c6fcef
 
 $id_user = $_REQUEST['nama'] ?? null;
 $id_mobil = $_REQUEST['id_modal'] ?? null;
 $review = $_REQUEST['review'] ?? null;
 
-<<<<<<< HEAD
-                // Check connection
-                if ($conn->connect_error) {
-                    die("Connection failed: " . $conn->connect_error);
-                }
-                //echo "Connected successfully";
-                //<div class="filter-wrap mt-4 d-flex flex-lg-row flex-column justify-content-lg-center row-gap-2 column-gap-3 mb-4"
-                //ata-aos="zoom-in" data-aos-duration="1000">
-                //<span class="filter-btn filter-active" data-filter="*">All</span>
-                //<span class="filter-btn" data-filter="filter-SUV">SUV</span>
-                //<span class="filter-btn" data-filter="filter-MPV">MPV</span>
-                //<span class="filter-btn" data-filter="filter-Hatchback">Hatchback</span>
-                //<span class="filter-btn" data-filter="filter-Sedan">Sedan</span>
-                //<span class="filter-btn" data-filter="filter-PickUp">PickUp</span>
-                //</div>
-                $a = 1;
-                $sql = "SELECT user.username as 'username', mobil.nama as 'nama', mobil.gambar as 'gambar', massage 
-=======
 $submit = "INSERT INTO review (id_user,id,massage)  VALUES ('$id_user','$id_mobil','$review')";
 mysqli_query($conn, $submit)
 
@@ -99,53 +42,11 @@ mysqli_query($conn, $submit)
                     <div class="row cars-container" data-aos="zoom-in" data-aos-duration="1000">
                         <?php
                         $sql = "SELECT tipe.nama as 'tipe', user.username as 'username', mobil.nama as 'nama', mobil.gambar as 'gambar', massage , rating
->>>>>>> a7326631bc79ff171851a12f6ce9dc3586c6fcef
                     FROM review
                     INNER join user on user.id_user = review.id_user
                     INNER Join mobil on mobil.id = review.id
                     INNER Join tipe on tipe.id_tipe = mobil.id_tipe";
 
-<<<<<<< HEAD
-                $review = $conn->query($sql);
-                foreach ($review as $r) { ?>
-                    <div class="col-xl-3 col-lg-4 col-md-6 col-12 cars-item d-flex align-items-stretch mt-4">
-                        <div class="cars-wrap justify-content-lg-stretch">
-                            <div class="cars-info justify-content-lg-stretch">
-                                <tr>
-                                    <th scope="row">
-                                        <!--<?= $a++; ?>-->
-                                    </th>
-                                    <td>
-                                        <h4>
-                                            <?= $r['username']; ?>
-                                        </h4>
-                                    </td>
-                                    <br>
-                                    <td>
-                                        <div class="cars-img">
-                                            <img src="img/cars/<?= $r['gambar']; ?>" alt="" class="img-fluid">
-                                        </div>
-                                        Mobil :
-                                        <h6><?= $r['nama']; ?></h6>
-                                    </td>
-                                    <br>
-                                    <td>
-                                        Review :
-                                        <h6><?= $r['massage']; ?></h6>
-                                    </td>
-                                    <br>
-                            </div>
-                        </div>
-                    </div>
-                    <br>
-
-
-                    </tr>
-                <?php } ?>
-            </tbody>
-        </div>
-
-=======
 $review = $conn->query($sql);
 $a = 1;
 foreach ($review as $r) { ?>
@@ -185,7 +86,6 @@ foreach ($review as $r) { ?>
             </tbody>
         </div>
 
->>>>>>> a7326631bc79ff171851a12f6ce9dc3586c6fcef
     </div>
 </div>
 </div>
