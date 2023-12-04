ae10d1cad5fe9193a7eca39f8feb44680b07fdfc
Celeron <aliefeila12@gmail.com>
2023-12-04 10:04:43 +0700

    ketinggalan dikit

--- reviewcontroller.php
+++ reviewcontroller.php
@@ -2,7 +2,9 @@
 <body>
 <div class="container">
     <?php
-    
+    $id_user = $_REQUEST['nama'] ?? null;
+    $id_mobil = $_REQUEST['id_modal'] ?? null;
+    $review =  $_REQUEST['review'] ?? null;
     $submit = "INSERT INTO review (id_user,id,massage)  VALUES ('$id_user','$id_mobil','$review')";
     mysqli_query($conn, $submit)
 
